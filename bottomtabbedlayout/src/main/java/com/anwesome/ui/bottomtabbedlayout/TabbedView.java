030d87e92b0d3087eac184b045ac4d52292cb158
Anwesh43 <anweshthecool0@gmail.com>
2017-04-06 18:09:09 +0530

    Adding TabbedView

---
+++ bottomtabbedlayout/src/main/java/com/anwesome/ui/bottomtabbedlayout/TabbedView.java
@@ -0,0 +1,17 @@
+package com.anwesome.ui.bottomtabbedlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 06/04/17.
+ */
+public class TabbedView extends View {
+    public TabbedView(Context context) {
+        super(context);
+    }
+    public TabbedView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+}
