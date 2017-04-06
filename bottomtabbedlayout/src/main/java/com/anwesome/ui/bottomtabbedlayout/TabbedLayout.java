cbdf59f664bd88898af0b7c2271371519915beec
Anwesh43 <anweshthecool0@gmail.com>
2017-04-06 18:01:41 +0530

    Adding TabbedLayout

---
+++ bottomtabbedlayout/src/main/java/com/anwesome/ui/bottomtabbedlayout/TabbedLayout.java
@@ -0,0 +1,23 @@
+package com.anwesome.ui.bottomtabbedlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by anweshmishra on 06/04/17.
+ */
+public class TabbedLayout extends ViewGroup {
+    public void onMeasure(int wspec,int hspec) {
+
+    }
+    public void onLayout(boolean reloaded,int a,int b,int w,int h) {
+
+    }
+    public TabbedLayout(Context context) {
+        super(context);
+    }
+    public TabbedLayout(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+}
